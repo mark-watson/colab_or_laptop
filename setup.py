8c8c4204841ab2c1bf32b87c5abc4ae6f6c3e61a
Mark Watson <markw@markwatson.com>
2024-10-18 14:39:16 -0700

    -

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='colab_or_laptop',
     version='0.1.0',
-    packages=find_packages(),
+    py_modules=['colab_or_laptop'],
     install_requires=[
         # List your project's dependencies here.
     ],
 )
-
